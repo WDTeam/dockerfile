bc83977cc5451f0997be392a556eeb4c121dfda6
windsdeng <windsdeng@gmail.com>
2016-09-28 11:44:29 +0800

    init

---
+++ crontab-debian/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+os=debian
+
+version=$1
+version=${version:-"latest"}
+
+app_component=crontab-example
+
+if [[ ! $version =~ ^[0-9]*\.[0-9]*\.[0-9]*$ ]] && [ "$version" != "latest" ]; then
+    echo "Incorrect syntax of the version"
+    exit 1
+fi
+
+docker build -t $app_component:$os-$version -f Dockerfile .
